c708f67d00e8d4bd031f6ca8986f7af4697bb23e
Bernie Forbes <besanegames@gmail.com>
2023-11-08 09:22:40 +1000

    starting bevy ECS

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new().add_startup_system(setup).run()
+}
+
+pub fn setup(mut commands: Commands) {
+    commands.spawn(Person {
+        name: "Alex".to_string(),
+    });
+}
+
+#[derive(Component)]
+pub struct Person {
+    pub name: String,
 }
