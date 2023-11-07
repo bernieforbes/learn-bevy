cfbaf6264055f89bb009e08b6ad07d387fb53697
Bernie Forbes <besanegames@gmail.com>
2023-11-08 09:39:54 +1000

    prints names

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,38 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_startup_system(setup).run()
+    App::new()
+        .add_startup_system(setup)
+        .add_system(print_names)
+        .run()
 }
 
 pub fn setup(mut commands: Commands) {
     commands.spawn(Person {
         name: "Alex".to_string(),
     });
+
+    commands.spawn(Person {
+        name: "Bob".to_string(),
+    });
+
+    commands.spawn(Person {
+        name: "Charlie".to_string(),
+    });
+
+    commands.spawn(Person {
+        name: "David".to_string(),
+    });
+
+    commands.spawn(Person {
+        name: "Ellen".to_string(),
+    });
+}
+
+pub fn print_names(person_query: Query<&Person>) {
+    for person in person_query.iter() {
+        println!("Name: {}", person.name);
+    }
 }
 
 #[derive(Component)]
