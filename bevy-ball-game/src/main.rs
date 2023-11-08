0bc6e79ba6248870352797cf4e8909284a5d7b97
Bernie Forbes <besanegames@gmail.com>
2023-11-08 21:39:07 +1000

    converted to bevy 0.12.0

--- bevy-ball-game/src/main.rs
+++ bevy-ball-game/src/main.rs
@@ -4,17 +4,22 @@ use rand::prelude::*;
 pub const PLAYER_SIZE: f32 = 64.0; // This is the player sprite size
 pub const PLAYER_SPEED: f32 = 500.0;
 pub const NUMBER_OF_ENEMIES: usize = 4;
+pub const ENEMY_SIZE: f32 = 64.0; // This is the enemy sprite size
 pub const ENEMY_SPEED: f32 = 200.0;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(spawn_camera)
-        .add_startup_system(spawn_player)
-        .add_startup_system(spawn_enemies)
-        .add_system(player_movement)
-        .add_system(enemy_movement)
-        .add_system(confine_player_movement)
+        .add_systems(Startup, (spawn_camera, spawn_player, spawn_enemies))
+        .add_systems(
+            Update,
+            (
+                player_movement,
+                confine_player_movement,
+                enemy_movement,
+                update_enemy_direction,
+            ),
+        )
         .run();
 }
 
@@ -138,10 +143,83 @@ pub fn confine_player_movement(
 }
 
 pub fn enemy_movement(mut enemy_query: Query<(&mut Transform, &Enemy)>, time: Res<Time>) {
-    // println!("enemy_movement");
     for (mut transform, enemy) in enemy_query.iter_mut() {
-        // println!("{:?}", transform);
         let direction = Vec3::new(enemy.direction.x, enemy.direction.y, 0.0);
         transform.translation += direction * ENEMY_SPEED * time.delta_seconds();
+    }
+}
+
+pub fn update_enemy_direction(
+    mut enemy_query: Query<(&Transform, &mut Enemy)>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    // audio: Res<Audio>,
+    // asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    let half_enemy_size = ENEMY_SIZE / 2.0; // 32.0
+    let x_min = 0.0 + half_enemy_size;
+    let x_max = window.width() - half_enemy_size;
+    let y_min = 0.0 + half_enemy_size;
+    let y_max = window.height() - half_enemy_size;
+
+    for (transform, mut enemy) in enemy_query.iter_mut() {
+        // let mut direction_changed = false;
+
+        let translation = transform.translation;
+        if translation.x < x_min || translation.x > x_max {
+            enemy.direction.x *= -1.0;
+            // direction_changed = true;
+        }
+        if translation.y < y_min || translation.y > y_max {
+            enemy.direction.y *= -1.0;
+            // direction_changed = true;
+        }
+
+        // // Play SFX
+        // if direction_changed {
+        //     // Play Sound Effect
+        //     let sound_effect_1 = asset_server.load("audio/pluck_001.ogg");
+        //     let sound_effect_2 = asset_server.load("audio/pluck_002.ogg");
+        //     // Randomly play one of the two sound effects.
+        //     let sound_effect = if random::<f32>() > 0.5 {
+        //         sound_effect_1
+        //     } else {
+        //         sound_effect_2
+        //     };
+        //     audio.play(sound_effect);
+        // }
+    }
+}
+
+pub fn confine_enemy_movement(
+    mut enemy_query: Query<&mut Transform, With<Enemy>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    let half_enemy_size = ENEMY_SIZE / 2.0;
+    let x_min = 0.0 + half_enemy_size;
+    let x_max = window.width() - half_enemy_size;
+    let y_min = 0.0 + half_enemy_size;
+    let y_max = window.height() - half_enemy_size;
+
+    for mut transform in enemy_query.iter_mut() {
+        let mut translation = transform.translation;
+
+        // Bound the enemy x position
+        if translation.x < x_min {
+            translation.x = x_min;
+        } else if translation.x > x_max {
+            translation.x = x_max;
+        }
+        // Bound the enemy y position
+        if translation.y < y_min {
+            translation.y = y_min;
+        } else if translation.y > y_max {
+            translation.y = y_max;
+        }
+
+        transform.translation = translation;
     }
 }
