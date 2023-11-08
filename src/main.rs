0b774ffbfbd42b20b67e572f12eb889f944c5a27
Bernie Forbes <besanegames@gmail.com>
2023-11-08 10:11:59 +1000

    added second query

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,46 @@ fn main() {
     App::new()
         .add_startup_system(setup)
         .add_system(print_names)
+        .add_system(people_with_jobs)
         .run()
 }
 
 pub fn setup(mut commands: Commands) {
-    commands.spawn(Person {
-        name: "Alex".to_string(),
-    });
+    commands.spawn((
+        Person {
+            name: "Alex".to_string(),
+        },
+        Employed { job: Job::Doctor },
+    ));
 
     commands.spawn(Person {
         name: "Bob".to_string(),
     });
 
-    commands.spawn(Person {
-        name: "Charlie".to_string(),
-    });
+    commands.spawn((
+        Person {
+            name: "Charlie".to_string(),
+        },
+        Employed {
+            job: Job::FireFighter,
+        },
+    ));
 
-    commands.spawn(Person {
-        name: "David".to_string(),
-    });
+    commands.spawn((
+        Person {
+            name: "David".to_string(),
+        },
+        Employed { job: Job::Lawyer },
+    ));
 
-    commands.spawn(Person {
-        name: "Ellen".to_string(),
-    });
+    commands.spawn((
+        Person {
+            name: "Ellen".to_string(),
+        },
+        Employed {
+            job: Job::FireFighter,
+        },
+    ));
 }
 
 pub fn print_names(person_query: Query<&Person>) {
@@ -35,7 +52,25 @@ pub fn print_names(person_query: Query<&Person>) {
     }
 }
 
+pub fn people_with_jobs(person_query: Query<&Person, With<Employed>>) {
+    for person in person_query.iter() {
+        println!("{} has a job.", person.name);
+    }
+}
+
 #[derive(Component)]
 pub struct Person {
     pub name: String,
+}
+
+#[derive(Component)]
+pub struct Employed {
+    pub job: Job,
+}
+
+#[derive(Debug)]
+pub enum Job {
+    Doctor,
+    FireFighter,
+    Lawyer,
 }
