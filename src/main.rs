c3165bca77d3343a831d1d0c5ed182ec0e83acdc
Bernie Forbes <besanegames@gmail.com>
2023-11-08 11:35:01 +1000

    added default plugins

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,21 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_startup_system(setup)
-        .add_system(print_names)
-        .add_system(people_with_jobs)
+        .add_plugins(DefaultPlugins)
+        .add_plugin(PeoplePlugin)
         .run()
+}
+
+pub struct PeoplePlugin;
+
+impl Plugin for PeoplePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(setup)
+            .add_system(print_names)
+            .add_system(people_with_jobs)
+            .add_system(people_ready_for_hire)
+            .add_system(person_does_job);
+    }
 }
 
 pub fn setup(mut commands: Commands) {
@@ -55,6 +66,23 @@ pub fn print_names(person_query: Query<&Person>) {
 pub fn people_with_jobs(person_query: Query<&Person, With<Employed>>) {
     for person in person_query.iter() {
         println!("{} has a job.", person.name);
+    }
+}
+
+pub fn people_ready_for_hire(person_query: Query<&Person, Without<Employed>>) {
+    for person in person_query.iter() {
+        println!("{} is ready for hire.", person.name);
+    }
+}
+
+pub fn person_does_job(person_query: Query<(&Person, &Employed)>) {
+    for (person, employed) in person_query.iter() {
+        let job_name = match employed.job {
+            Job::Doctor => "Doctor",
+            Job::FireFighter => "FireFighter",
+            Job::Lawyer => "Lawyer",
+        };
+        println!("{0} is a {1}.", person.name, job_name);
     }
 }
 
