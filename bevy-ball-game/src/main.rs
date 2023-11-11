a6314552996683595a28173503467813415bf600
Bernie Forbes <besanegames@gmail.com>
2023-11-11 23:50:00 +1000

    code cleanup

--- bevy-ball-game/src/main.rs
+++ bevy-ball-game/src/main.rs
@@ -196,12 +196,15 @@ pub fn player_movement(
         if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
             direction += Vec3::new(-1.0, 0.0, 0.0);
         }
+
         if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
             direction += Vec3::new(1.0, 0.0, 0.0);
         }
+
         if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
             direction += Vec3::new(0.0, 1.0, 0.0);
         }
+
         if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
             direction += Vec3::new(0.0, -1.0, 0.0);
         }
