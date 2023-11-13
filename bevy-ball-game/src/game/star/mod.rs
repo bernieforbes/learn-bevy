be5e9c0972e3d5a055b9bf1a5fdb9733e7004790
Bernie Forbes <besanegames@gmail.com>
2023-11-13 20:45:55 +1000

    added game states to star/mod.rs

--- bevy-ball-game/src/game/star/mod.rs
+++ bevy-ball-game/src/game/star/mod.rs
@@ -7,6 +7,9 @@ mod systems;
 use resources::*;
 use systems::*;
 
+use crate::game::SimulationState;
+use crate::AppState;
+
 pub const NUMBER_OF_STARS: usize = 10;
 pub const STAR_SIZE: f32 = 30.0; // This is the star sprite size
 
@@ -15,7 +18,12 @@ pub struct StarPlugin;
 impl Plugin for StarPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<StarSpawnTimer>()
-            .add_systems(Startup, spawn_stars)
-            .add_systems(Update, (tick_star_spawn_timer, spawn_stars_over_time));
+            .add_systems(OnEnter(AppState::Game), spawn_stars)
+            .add_systems(
+                Update,
+                (tick_star_spawn_timer, spawn_stars_over_time)
+                    .run_if(in_state(AppState::Game))
+                    .run_if(in_state(SimulationState::Running)),
+            );
     }
 }
