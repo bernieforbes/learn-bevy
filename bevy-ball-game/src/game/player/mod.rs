8f3d32068e6eb4fd127d4a5d5c9257af76951689
Bernie Forbes <besanegames@gmail.com>
2023-11-13 21:30:15 +1000

    Player can now only move if game state is correct

--- bevy-ball-game/src/game/player/mod.rs
+++ bevy-ball-game/src/game/player/mod.rs
@@ -4,12 +4,21 @@ mod systems;
 use bevy::prelude::*;
 use systems::*;
 
+use super::SimulationState;
+use crate::AppState;
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_player)
-            .add_systems(Update, (player_movement, confine_player_movement).chain())
+            .add_systems(
+                Update,
+                (player_movement, confine_player_movement)
+                    .run_if(in_state(AppState::Game))
+                    .run_if(in_state(SimulationState::Running))
+                    .chain(),
+            )
             .add_systems(Update, (enemy_hit_player, player_hit_star));
     }
 }
