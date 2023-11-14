b559d056e4bcbb089c24c93e9284967ea1aa3492
Bernie Forbes <besanegames@gmail.com>
2023-11-14 23:40:00 +1000

    Buttons now entering game mode and exiting application as expected

--- bevy-ball-game/src/main_menu/systems/interactions.rs
+++ bevy-ball-game/src/main_menu/systems/interactions.rs
@@ -1,18 +1,22 @@
+use bevy::app::AppExit;
 use bevy::prelude::*;
 
 use crate::main_menu::components::*;
 use crate::main_menu::styles::{HOVERED_BUTTON_COLOR, NORMAL_BUTTON_COLOR, PRESSED_BUTTON_COLOR};
+use crate::AppState;
 
 pub fn interact_with_play_button(
     mut button_query: Query<
         (&Interaction, &mut BackgroundColor),
         (Changed<Interaction>, With<PlayButton>),
     >,
+    mut next_app_state: ResMut<NextState<AppState>>,
 ) {
     if let Ok((interaction, mut background_color)) = button_query.get_single_mut() {
         match *interaction {
             Interaction::Pressed => {
                 *background_color = PRESSED_BUTTON_COLOR.into();
+                next_app_state.set(AppState::Game);
             }
             Interaction::Hovered => {
                 *background_color = HOVERED_BUTTON_COLOR.into();
@@ -25,6 +29,7 @@ pub fn interact_with_play_button(
 }
 
 pub fn interact_with_quit_button(
+    mut app_exit_event_writer: EventWriter<AppExit>,
     mut button_query: Query<
         (&Interaction, &mut BackgroundColor),
         (Changed<Interaction>, With<QuitButton>),
@@ -34,6 +39,7 @@ pub fn interact_with_quit_button(
         match *interaction {
             Interaction::Pressed => {
                 *background_color = PRESSED_BUTTON_COLOR.into();
+                app_exit_event_writer.send(AppExit);
             }
             Interaction::Hovered => {
                 *background_color = HOVERED_BUTTON_COLOR.into();
