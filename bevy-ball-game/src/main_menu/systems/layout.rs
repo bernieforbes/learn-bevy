146bc51e369c256b6ac998fb7e0dde9844c1692b
Bernie Forbes <besanegames@gmail.com>
2023-11-14 22:31:06 +1000

    Added title to main menu

--- bevy-ball-game/src/main_menu/systems/layout.rs
+++ bevy-ball-game/src/main_menu/systems/layout.rs
@@ -4,7 +4,7 @@ use crate::main_menu::components::*;
 use crate::main_menu::styles::*;
 
 pub fn spawn_main_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let main_menu_entity = build_main_menu(&mut commands, &asset_server);
+    build_main_menu(&mut commands, &asset_server);
 }
 
 pub fn despawn_main_menu(mut commands: Commands, main_menu_query: Query<Entity, With<MainMenu>>) {
@@ -27,14 +27,77 @@ pub fn build_main_menu(commands: &mut Commands, asset_server: &Res<AssetServer>)
                     column_gap: Val::Px(8.0),
                     ..default()
                 },
-                background_color: Color::RED.into(),
+                // background_color: Color::RED.into(),
                 ..default()
             },
             MainMenu {},
         ))
         .with_children(|parent| {
             // === Title ===
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        flex_direction: FlexDirection::Row,
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        width: Val::Px(600.0),
+                        height: Val::Px(80.0),
+                        ..default()
+                    },
+                    ..default()
+                })
+                .with_children(|parent| {
+                    // Image 1
+                    parent.spawn(ImageBundle {
+                        style: Style {
+                            width: Val::Px(64.0),
+                            height: Val::Px(64.0),
+                            margin: UiRect::new(
+                                Val::Px(8.0),
+                                Val::Px(8.0),
+                                Val::Px(8.0),
+                                Val::Px(8.0),
+                            ),
+                            ..default()
+                        },
+                        image: asset_server.load("sprites/ball_blue_large.png").into(),
+                        ..default()
+                    });
 
+                    // Text
+                    parent.spawn(TextBundle {
+                        text: Text {
+                            sections: vec![TextSection::new(
+                                "Bevy Ball Game",
+                                TextStyle {
+                                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                    font_size: 64.0,
+                                    color: Color::WHITE,
+                                },
+                            )],
+                            alignment: TextAlignment::Center,
+                            ..default()
+                        },
+                        ..default()
+                    });
+
+                    // Image 2
+                    parent.spawn(ImageBundle {
+                        style: Style {
+                            width: Val::Px(64.0),
+                            height: Val::Px(64.0),
+                            margin: UiRect::new(
+                                Val::Px(8.0),
+                                Val::Px(8.0),
+                                Val::Px(8.0),
+                                Val::Px(8.0),
+                            ),
+                            ..default()
+                        },
+                        image: asset_server.load("sprites/ball_red_large.png").into(),
+                        ..default()
+                    });
+                });
             // === Play Button ===
             parent
                 .spawn((
